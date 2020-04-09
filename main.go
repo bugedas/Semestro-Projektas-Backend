297e48a03cbf021fd5c3b7d971395d46fef5c0bf
Airidasz <61290246+Airidasz@users.noreply.github.com>
2020-04-09 11:50:23 +0300

    Moved stuff from main
    Moved account and handlers from main

--- main.go
+++ main.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"crypto/sha1"
-	"encoding/hex"
 	"encoding/json"
-	"errors"
 	"log"
 	"net/http"
 	"regexp"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/gofrs/uuid"
 	"github.com/jinzhu/gorm"
 	"github.com/wader/gormstore"
-	"golang.org/x/crypto/pbkdf2"
 )
 
 // Global variables -------------------------------------------
@@ -24,100 +19,8 @@ var passwordRegex *regexp.Regexp
 
 // ------------------------------------------------------------
 
-type User struct {
-	gorm.Model
-	Username string
-	Password string
-	Salt     string
-}
-
 type Response struct {
 	Data string `json:"data"`
-}
-
-//GenerateSalt creates a pseudorandom salt used in password salting
-func GenerateSalt() string {
-	salt, _ := uuid.NewV4()
-
-	return hex.EncodeToString(salt.Bytes())
-}
-
-//GenerateSecurePassword generates a password using PBKDF2 standard
-func GenerateSecurePassword(password string, salt string) string {
-	hashedPassword := pbkdf2.Key([]byte(password), []byte(salt), 4096, 32, sha1.New)
-
-	return hex.EncodeToString(hashedPassword)
-}
-
-//CheckNameAvailability checks if a username is available
-func CheckNameAvailability(username string) error {
-	var user User
-
-	db.Find(&user, "username = ?", username)
-
-	if user.Username != "" {
-		return errors.New("Username exists")
-	}
-
-	return nil
-}
-
-//CreateNewAccount creates an account if the sent data
-//is correctly formatted
-func CreateNewAccount(username string, password string, repeatedPassword string) (httpStatus int, err error) {
-	err = CheckNameAvailability(username)
-	if err != nil {
-		return http.StatusNotAcceptable, err
-	}
-
-	err = ComparePasswords(password, repeatedPassword)
-	if err != nil {
-		return http.StatusBadRequest, err
-	}
-
-	salt := GenerateSalt()
-	hashedPassword := GenerateSecurePassword(password, salt)
-
-	user := User{
-		Username: username,
-		Password: hashedPassword,
-		Salt:     salt,
-	}
-	db.Debug().Create(&user)
-	db.Save(&user)
-
-	return http.StatusCreated, nil
-}
-
-//ComparePasswords checks that, while registering a new account,
-//the password matches the repeated password, is atleast 8 characters long and
-//contains at least one number and one capital letter
-func ComparePasswords(passwordOne string, passwordTwo string) error {
-	if passwordOne != passwordTwo {
-		return errors.New("Passwords do not match")
-	}
-
-	if len(passwordOne) < 8 {
-		return errors.New("Passwords too short")
-	}
-
-	if passwordRegex.MatchString(passwordOne) != true {
-		return errors.New("Passwords needs to contain at least one number and one capital letter")
-	}
-
-	return nil
-}
-
-//RefreshToken refreshes authentication token WORK IN PROGRESS not even close to complete
-func RefreshToken(w http.ResponseWriter, r *http.Request) {
-	refreshSession, _ := sessionStore.Get(r, "refresh-token")
-
-	authSession, _ := sessionStore.Get(r, "auth-token")
-
-	authSession.Values["userID"] = refreshSession.Values["userID"]
-	authSession.Options.MaxAge = 60
-	authSession.Save(r, w)
-
 }
 
 //JsonResponse sends a json response to user based on message
@@ -149,6 +52,9 @@ func main() {
 	if !db.HasTable(&User{}) {
 		db.CreateTable(&User{})
 	}
+	if !db.HasTable(&Event{}) {
+		db.CreateTable(&Event{})
+	}
 
 	//Creates a table in the database for storing sessions
 	//and sets a cleanup time
@@ -157,6 +63,4 @@ func main() {
 	go sessionStore.PeriodicCleanup(time.Minute, quit)
 
 	HandleFunctions()
-
-	log.Println("Server started on port 8000")
 }
