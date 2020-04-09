793bc473ea1228b7f8a5aab2915c4d6b46ab80b1
Airidasz <61290246+Airidasz@users.noreply.github.com>
2020-04-09 11:50:44 +0300

    Moved account features
    Moved account features to account.go

--- handlers.go
+++ handlers.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -30,85 +29,25 @@ func LandingPageHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello world"))
 }
 
-func LoginPageHandler(w http.ResponseWriter, r *http.Request) {
-	session, _ := sessionStore.Get(r, "auth-token")
+//RefreshToken refreshes authentication token WORK IN PROGRESS not even close to complete
+func RefreshToken(w http.ResponseWriter, r *http.Request) {
+	refreshSession, _ := sessionStore.Get(r, "refresh-token")
 
-	if session.Values["userID"] != nil {
-		w.WriteHeader(http.StatusUnauthorized)
-		response := Response{"Already logged in"}
-		JsonResponse(response, w)
-		return
-	}
+	authSession, _ := sessionStore.Get(r, "auth-token")
 
-	userRequestData := struct {
-		Username string `json: "username"`
-		Password string `json: "password"`
-	}{"", ""}
+	authSession.Values["userID"] = refreshSession.Values["userID"]
+	authSession.Options.MaxAge = 60
+	authSession.Save(r, w)
 
-	json.NewDecoder(r.Body).Decode(&userRequestData)
-
-	var userDatabaseData User
-
-	db.Find(&userDatabaseData, "username = ?", userRequestData.Username)
-
-	if userDatabaseData.Username == "" {
-		w.WriteHeader(http.StatusUnauthorized)
-		response := Response{"Bad credentials"}
-		JsonResponse(response, w)
-		return
-	}
-
-	hashedPassword := GenerateSecurePassword(userRequestData.Password, userDatabaseData.Salt)
-
-	if hashedPassword != userDatabaseData.Password {
-		w.WriteHeader(http.StatusUnauthorized)
-		response := Response{"Bad credentials"}
-		JsonResponse(response, w)
-		return
-	}
-
-	session.Values["userID"] = userDatabaseData.ID
-
-	session.Save(r, w)
-
-	w.WriteHeader(http.StatusAccepted)
-	response := Response{"Login successful"}
-	JsonResponse(response, w)
-	return
-}
-
-//RegisterPageHandler decodes user sent in data, verifies that
-//it is formatted correctly, and tries to create an account in
-//the database
-func RegisterPageHandler(w http.ResponseWriter, r *http.Request) {
-	user := struct {
-		Username       string `json: "username"`
-		Password       string `json: "password"`
-		RepeatPassword string `json: "repeatPassword"`
-	}{"", "", ""}
-
-	err := json.NewDecoder(r.Body).Decode(&user)
-
-	res, err := CreateNewAccount(user.Username, user.Password, user.RepeatPassword)
-
-	w.WriteHeader(res)
-
-	if err != nil {
-		response := Response{err.Error()}
-		JsonResponse(response, w)
-		return
-	}
-
-	response := Response{"Account created successfully"}
-	JsonResponse(response, w)
-	return
 }
 
 func HandleFunctions() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", LandingPageHandler)
-	r.HandleFunc("/login", LoginPageHandler).Methods("POST")
-	r.HandleFunc("/register", RegisterPageHandler).Methods("POST")
+	r.HandleFunc("/login", Login).Methods("POST")
+	r.HandleFunc("/account", RegisterNewAccount).Methods("POST")
 	r.HandleFunc("/resource", ProtectedHandler)
+	r.HandleFunc("/events", CreateEvent).Methods("POST")
+	//r.HandleFunc("/events", GetEventsHandler).Methods("GET")
 	http.ListenAndServe(":8000", r)
 }
