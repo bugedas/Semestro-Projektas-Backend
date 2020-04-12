ac38ba05a824c2a7a7ac2c2548302ab0bc874c6f
Airidasz <61290246+Airidasz@users.noreply.github.com>
2020-04-12 18:45:01 +0300

    ability to join events
    Added ability to join events

--- events.go
+++ events.go
@@ -31,3 +31,32 @@ func CreateEvent(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	return
 }
+func JoinEvent(w http.ResponseWriter, r *http.Request) {
+	session, _ := sessionStore.Get(r, "auth-token")
+
+	if session.Values["userID"] == nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	event := struct {
+		ID int `json: "id"`
+	}{}
+
+	json.NewDecoder(r.Body).Decode(&event)
+
+	var user User
+	db.First(&user, session.Values["userID"].(uint))
+
+	var eventD Event
+	db.Preload("Users").First(&eventD, "id = ?", event.ID)
+
+	if eventD.ID == 0 || user.ID == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	db.Model(&eventD).Association("Users").Append(&user)
+
+	w.WriteHeader(http.StatusOK)
+	return
+}
