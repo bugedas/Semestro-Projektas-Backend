deb4337deb4bbfc00228ca3294141fb11dd57ecf
Airidasz <61290246+Airidasz@users.noreply.github.com>
2020-04-13 09:43:06 +0300

    changed get events
    changed get events so that it uses url  keys instead of body info for the filters

--- events.go
+++ events.go
@@ -11,15 +11,25 @@ import (
 
 type Event struct {
 	gorm.Model
-	Creator     int     `json: "creator"`
+	Creator     User `gorm:"foreignkey:CreatorID"`
+	CreatorID   uint
 	Description string  `json: "description"`
 	Location    string  `json: "location"`
 	Users       []*User `gorm:"many2many:events_joined;"`
 }
 
 func CreateEvent(w http.ResponseWriter, r *http.Request) {
-	var newEvent Event
+	var user User
+	session, _ := sessionStore.Get(r, "auth-token")
 
+	if session.Values["userID"] == nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	db.First(&user, session.Values["userID"].(uint))
+	var newEvent Event
+	newEvent.Creator = user
 	err := json.NewDecoder(r.Body).Decode(&newEvent)
 
 	if err != nil {
@@ -28,29 +38,9 @@ func CreateEvent(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	db.Model(&newEvent).AddForeignKey("creator_id", "users(id)", "RESTRICT", "RESTRICT")
 	db.Create(&newEvent)
 	w.WriteHeader(http.StatusCreated)
-	return
-}
-
-func GetEvents(w http.ResponseWriter, r *http.Request) {
-	var events []Event
-
-	var eventFilter Event
-
-	err := json.NewDecoder(r.Body).Decode(&eventFilter)
-
-	log.Println(eventFilter)
-	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	db.Where("location = ?", eventFilter.Location).Select("creator, description, location").Find(&events)
-
-	JSONResponse(events, w)
-	w.WriteHeader(http.StatusOK)
 	return
 }
 
@@ -70,16 +60,72 @@ func JoinEvent(w http.ResponseWriter, r *http.Request) {
 	var user User
 	db.First(&user, session.Values["userID"].(uint))
 
-	var eventD Event
-	db.Preload("Users").First(&eventD, "id = ?", event.ID)
+	var selectedEvent Event
+	db.Preload("Users").First(&selectedEvent, "id = ?", event.ID)
 
-	if eventD.ID == 0 || user.ID == 0 {
+	if selectedEvent.ID == 0 || user.ID == 0 {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	db.Model(&eventD).Association("Users").Append(&user)
+	db.Model(&selectedEvent).Association("Users").Append(&user)
 
+	w.WriteHeader(http.StatusOK)
+	return
+}
+
+func LeaveEvent(w http.ResponseWriter, r *http.Request) {
+	session, _ := sessionStore.Get(r, "auth-token")
+
+	if session.Values["userID"] == nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	event := struct {
+		ID int `json: "id"`
+	}{}
+
+	json.NewDecoder(r.Body).Decode(&event)
+
+	var user User
+	db.First(&user, session.Values["userID"].(uint))
+
+	var selectedEvent Event
+	db.Preload("Users").First(&selectedEvent, "id = ?", event.ID)
+
+	if selectedEvent.ID == 0 || user.ID == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	db.Model(&selectedEvent).Association("Users").Delete(&user)
+
+	w.WriteHeader(http.StatusOK)
+	return
+}
+
+func GetEvents(w http.ResponseWriter, r *http.Request) {
+	keys := r.URL.Query()
+	location := keys.Get("location")
+	creatorID := keys.Get("creatorID")
+	var events []Event
+
+	tx := db.Table("events")
+
+	if location != "" {
+		tx = tx.Where("location = ?", location)
+	}
+	if creatorID != "" {
+		tx = tx.Where("creator = ?", creatorID)
+	}
+
+	tx.Preload("Users").Find(&events)
+
+	if len(events) == 0 {
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	JSONResponse(events, w)
 	w.WriteHeader(http.StatusOK)
 	return
 }
