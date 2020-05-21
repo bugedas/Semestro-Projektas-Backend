f7b881858fbe4f42e77ecd6a6cc1744b2047c1f9
Airidasz <61290246+Airidasz@users.noreply.github.com>
2020-05-21 16:14:43 +0300

    Added gorm settings

--- account.go
+++ account.go
@@ -15,12 +15,12 @@ import (
 
 type User struct {
 	gorm.Model
-	Email       string
-	Username    string
-	Gender      string
-	Description string
-	Password    string   `gorm:"PRELOAD:false"`
-	Salt        string   `gorm:"PRELOAD:false"`
+	Email       string   `gorm:"size:50;not null`
+	Username    string   `gorm:"size:30;`
+	Gender      string   `gorm:"size:20`
+	Description string   `gorm:"size:255;not null`
+	Password    string   `gorm:"PRELOAD:false;not null"`
+	Salt        string   `gorm:"PRELOAD:false;size:64;not null"`
 	Events      []*Event `gorm:"many2many:events_joined;"`
 }
 
@@ -117,7 +117,7 @@ func GetAccountInfo(w http.ResponseWriter, r *http.Request) {
 	}
 
 	var user User
-	db.Select("username, gender, description,email").First(&user, session.Values["userID"].(uint))
+	db.Select("username, gender, description, email").First(&user, session.Values["userID"].(uint))
 
 	JSONResponse(user, w)
 
